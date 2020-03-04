34874fb1b59584526891c7ec440b016ff2dcc671
Kotaro Sugawara <kotarella1110@gmail.com>
2020-03-04 13:44:09 +0900

    feat(memo): add useCustomCompareMemo (#3)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as useCustomCompareEffect } from './useCustomCompareEffect';
 export { default as useCustomCompareCallback } from './useCustomCompareCallback';
+export { default as useCustomCompareMemo } from './useCustomCompareMemo';
 export * from './types';
