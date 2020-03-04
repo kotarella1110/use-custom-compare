e6eec4eb046b09756349cf543d270244b8a1fc9b
Kotaro Sugawara <kotarella1110@gmail.com>
2020-03-04 13:37:31 +0900

    feat(callback): add useCustomCompareCallback

--- tests/useCustomCompareCallback.test.ts
+++ tests/useCustomCompareCallback.test.ts
@@ -1,43 +1,43 @@
 import { renderHook } from '@testing-library/react-hooks';
 import dequal from 'dequal';
-import { useCustomCompareEffect } from '../src';
+import { useCustomCompareCallback } from '../src';
 
 jest.spyOn(console, 'warn').mockImplementation(() => {});
 
-describe('useCustomCompareEffect', () => {
+describe('useCustomCompareCallback', () => {
   it('should output a warning message to console with an empty array deps', () => {
     renderHook(() =>
-      useCustomCompareEffect(
+      useCustomCompareCallback(
         () => {},
         [],
         () => true,
       ),
     );
     expect(console.warn).toHaveBeenCalledWith(
-      'useCustomCompareEffect should not be used with no dependencies. Use React.useEffect instead.',
+      'useCustomCompareCallback should not be used with no dependencies. Use React.useCallback instead.',
     );
   });
 
   it('should output a warning message to console with an array deps of only primitive values', () => {
     renderHook(() =>
-      useCustomCompareEffect(
+      useCustomCompareCallback(
         () => {},
         [true, 1, 'string'],
         () => true,
       ),
     );
     expect(console.warn).toHaveBeenCalledWith(
-      'useCustomCompareEffect should not be used with dependencies that are all primitive values. Use React.useEffect instead.',
+      'useCustomCompareCallback should not be used with dependencies that are all primitive values. Use React.useCallback instead.',
     );
   });
 
   it('should output a warning message to console with a depsAreEqual of primitive value', () => {
     renderHook(() =>
       // @ts-ignore
-      useCustomCompareEffect(() => {}, [1, { a: 'b' }, true], 1),
+      useCustomCompareCallback(() => {}, [1, { a: 'b' }, true], 1),
     );
     expect(console.warn).toHaveBeenCalledWith(
-      'useCustomCompareEffect should be used with depsEqual callback for comparing deps list',
+      'useCustomCompareCallback should be used with depsEqual callback for comparing deps list',
     );
   });
 
@@ -46,14 +46,14 @@ describe('useCustomCompareEffect', () => {
     process.env.NODE_ENV = 'production';
 
     renderHook(() =>
-      useCustomCompareEffect(
+      useCustomCompareCallback(
         () => {},
         [],
         () => true,
       ),
     );
     renderHook(() =>
-      useCustomCompareEffect(
+      useCustomCompareCallback(
         () => {},
         [true, 1, 'string'],
         () => true,
@@ -61,7 +61,7 @@ describe('useCustomCompareEffect', () => {
     );
     renderHook(() =>
       // @ts-ignore
-      useCustomCompareEffect(() => {}, [1, { a: 'b' }, true], 1),
+      useCustomCompareCallback(() => {}, [1, { a: 'b' }, true], 1),
     );
     expect(console.warn).not.toBeCalled();
 
@@ -70,46 +70,52 @@ describe('useCustomCompareEffect', () => {
 
   it('should handle changing deps as expected', () => {
     let deps = [1, { a: 'b' }, true];
-    const effect = jest.fn();
-    const { rerender } = renderHook(() =>
-      useCustomCompareEffect(effect, deps, dequal),
+    let prevCallback;
+    let callback = () => {};
+    const { rerender, result } = renderHook(() =>
+      useCustomCompareCallback(callback, deps, dequal),
     );
 
-    expect(effect).toHaveBeenCalledTimes(1);
-    effect.mockClear();
+    expect(result.current).toBe(callback);
 
     // no change
+    prevCallback = result.current;
+    callback = () => {};
     rerender();
-    expect(effect).toHaveBeenCalledTimes(0);
-    effect.mockClear();
+    expect(result.current).toBe(prevCallback);
 
     // no change (new object with same properties)
     deps = [1, { a: 'b' }, true];
+    prevCallback = result.current;
+    callback = () => {};
     rerender();
-    expect(effect).toHaveBeenCalledTimes(0);
-    effect.mockClear();
+    expect(result.current).toBe(prevCallback);
 
     // change (new primitive value)
     deps = [2, { a: 'b' }, true];
+    prevCallback = result.current;
+    callback = () => {};
     rerender();
-    expect(effect).toHaveBeenCalledTimes(1);
-    effect.mockClear();
+    expect(result.current).not.toBe(prevCallback);
 
     // no change
+    prevCallback = result.current;
+    callback = () => {};
     rerender();
-    expect(effect).toHaveBeenCalledTimes(0);
-    effect.mockClear();
+    expect(result.current).toBe(prevCallback);
 
     // change (new primitive value)
+    prevCallback = result.current;
+    callback = () => {};
     deps = [1, { a: 'b' }, false];
     rerender();
-    expect(effect).toHaveBeenCalledTimes(1);
-    effect.mockClear();
+    expect(result.current).not.toBe(prevCallback);
 
     // change (new properties on object)
+    prevCallback = result.current;
+    callback = () => {};
     deps = [1, { a: 'c' }, false];
     rerender();
-    expect(effect).toHaveBeenCalledTimes(1);
-    effect.mockClear();
+    expect(result.current).not.toBe(prevCallback);
   });
 });
