6c5820c0b70a50e08b23865fa6ffaa86e167aef9
Kotaro Sugawara <kotarella1110@gmail.com>
2020-03-07 01:52:43 +0900

    build(rollup): updated config

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,53 @@
 import external from 'rollup-plugin-peer-deps-external';
 import typescript from 'rollup-plugin-typescript2';
+import sourcemaps from 'rollup-plugin-sourcemaps';
 import { terser } from 'rollup-plugin-terser';
 import filesize from 'rollup-plugin-filesize';
 import pkg from './package.json';
 
-export default {
-  input: 'src/index.ts',
-  plugins: [
-    external(),
-    typescript({
-      clean: true,
-    }),
-    terser(),
-    filesize(),
-  ],
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      name: 'useCustomCompare',
+      file: pkg.unpkg,
+      format: 'umd',
+      sourcemap: true,
+      globals: {
+        react: 'React',
+      },
     },
-    {
-      file: pkg.module,
-      format: 'es',
-    },
-  ],
-};
+    plugins: [
+      external(),
+      typescript({
+        clean: true,
+      }),
+      sourcemaps(),
+      terser(),
+      filesize(),
+    ],
+  },
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: pkg.module,
+        format: 'es',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      external(),
+      typescript({
+        clean: true,
+      }),
+      sourcemaps(),
+      filesize(),
+    ],
+  },
+];
